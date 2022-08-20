40b1ad5c42f5061b405a990e8df4912834aec310
zhaoss <18519742475@163.com>
2022-08-20 09:33:27 +0800

    第一次数据的提交

---
+++ test-module/src/main/java/com/sz/testmodule/entity/Student.java
@@ -0,0 +1,10 @@
+package com.sz.testmodule.entity;
+
+/**
+ * @Auther: sunz
+ * @Date: 2022/8/20 - 08 - 20 - 9:25
+ * @Description: com.sz.testmodule.entity
+ * @version: 1.0
+ */
+public class Student {
+}
